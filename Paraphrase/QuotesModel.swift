9c58e7eb5b9f89dc8831b6e326393c5146c30b5a
Tadeh <tadeh19@gmail.com>
2020-05-10 11:39:28 +0430

    Finish Part1: How to refactor your code to add tests

---
+++ Paraphrase/QuotesModel.swift
@@ -0,0 +1,90 @@
+//
+//  QuotesModel.swift
+//  Paraphrase
+//
+//  Created by Tadeh Alexani on 5/10/20.
+//  Copyright © 2020 Hacking with Swift. All rights reserved.
+//
+
+import Foundation
+import GameplayKit
+
+struct QuotesModel {
+  private var quotes = [Quote]()
+  var randomSource: GKRandomSource?
+  
+  var count: Int {
+    return quotes.count
+  }
+  
+  init(testing: Bool = false) {
+    
+    if testing {
+      randomSource = GKMersenneTwisterRandomSource(seed: 1)
+    } else {
+      randomSource = GKMersenneTwisterRandomSource()
+    }
+    
+    // load our quote data
+    let defaults = UserDefaults.standard
+    let quoteData : Data
+    
+    if !testing, let savedQuotes = defaults.data(forKey: "SavedQuotes") {
+      // we have saved quotes; use them
+      SwiftyBeaver.info("Loading saved quotes")
+      quoteData = savedQuotes
+    } else {
+      // no saved quotes; load the default initial quotes
+      SwiftyBeaver.info("No saved quotes")
+      let path = Bundle.main.url(forResource: "initial-quotes", withExtension: "json")!
+      quoteData = try! Data(contentsOf: path)
+    }
+    
+    let decoder = JSONDecoder()
+    quotes = try! decoder.decode([Quote].self, from: quoteData)
+  }
+  
+  func random() -> Quote? {
+    guard !quotes.isEmpty else { return nil }
+    
+    let randomNumber = randomSource?.nextInt(upperBound: quotes.count) ?? 0
+    return quotes[randomNumber]
+  }
+  
+  func quote(at position: Int) -> Quote {
+    return quotes[position]
+  }
+  
+  mutating func add(_ quote: Quote) {
+    quotes.append(quote)
+    save()
+  }
+  
+  mutating func remove(at index: Int) {
+    quotes.remove(at: index)
+    save()
+  }
+  
+  mutating func replace(at index: Int, with quote: Quote) {
+    if quote.author.isEmpty && quote.text.isEmpty {
+      // if no text was entered just delete the quote
+      SwiftyBeaver.info("Removing empty quote")
+      quotes.remove(at: index)
+    } else {
+      // replace our existing quote with this new one then save
+      SwiftyBeaver.info("Replacing quote at index \(index)")
+      quotes[index] = quote
+    }
+    save()
+  }
+  
+  func save() {
+    let defaults = UserDefaults.standard
+    let encoder = JSONEncoder()
+    
+    let data = try! encoder.encode(quotes)
+    defaults.set(data, forKey: "SavedQuotes")
+    SwiftyBeaver.info("Quotes saved")
+  }
+  
+}
--- Paraphrase/ShowQuoteViewController.swift
+++ Paraphrase/ShowQuoteViewController.swift
@@ -9,53 +9,36 @@
 import UIKit
 
 class ShowQuoteViewController: UIViewController {
-    @IBOutlet var quoteLabel: UILabel!
-
-    var quote : Quote?
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        guard let quote = quote else {
-            navigationController?.popToRootViewController(animated: true)
-            return
-        }
-
-        title = quote.author
-        navigationItem.largeTitleDisplayMode = .never
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareQuote))
-
-        // format the text and author of this quote
-        var textAttributes = [NSAttributedStringKey: Any]()
-        var authorAttributes = [NSAttributedStringKey: Any]()
-
-        if let quoteFont = UIFont(name: "Georgia", size: 24) {
-            let fontMetrics = UIFontMetrics(forTextStyle: .headline)
-            textAttributes[.font] = fontMetrics.scaledFont(for: quoteFont)
-        }
-
-        if let authorFont = UIFont(name: "Georgia-Italic", size: 16) {
-            let fontMetrics = UIFontMetrics(forTextStyle: .body)
-            authorAttributes[.font] = fontMetrics.scaledFont(for: authorFont)
-        }
-
-        let finishedQuote = NSMutableAttributedString(string: quote.text, attributes: textAttributes)
-        let authorString = NSAttributedString(string: "\n\n\(quote.author)", attributes: authorAttributes)
-        finishedQuote.append(authorString)
-
-        quoteLabel.attributedText = finishedQuote
+  @IBOutlet var quoteLabel: UILabel!
+  
+  var quote : Quote?
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    guard let quote = quote else {
+      navigationController?.popToRootViewController(animated: true)
+      return
     }
-
-    @objc func shareQuote() {
-        guard let quote = quote else {
-            return
-        }
-
-        // format the quote neatly and share it
-        let fullText = "\"\(quote.text)\"\n   — \(quote.author)"
-        let activity = UIActivityViewController(activityItems: [fullText], applicationActivities: nil)
-        present(activity, animated: true)
-
-        SwiftyBeaver.info("Sharing quote \(fullText)")
+    
+    title = quote.author
+    navigationItem.largeTitleDisplayMode = .never
+    navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareQuote))
+    
+    // format the text and author of this quote
+    quoteLabel.attributedText = quote.attributedString
+  }
+  
+  @objc func shareQuote() {
+    guard let quote = quote else {
+      return
     }
+    
+    // format the quote neatly and share it
+    let fullText = quote.multiLine
+    let activity = UIActivityViewController(activityItems: [fullText], applicationActivities: nil)
+    present(activity, animated: true)
+    
+    SwiftyBeaver.info("Sharing quote \(fullText)")
+  }
 }
